da4c3fa4cdf3417e0c079bbf5738e3cee20233af
Rémy Peru <peru.remy@gmail.com>
2015-03-03 16:32:23 +0100

    switch from 6to5 to babel & use a better way to write to a file with browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,19 @@
 "use strict";
 
 var gulp = require('gulp');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
-var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync');
 var del = require('del');
-var to5ify = require("6to5ify");
+var babelify = require("babelify");
+var fs = require("fs");
 
 gulp.task('javascript', function() {
-  return browserify('./src/index.js')
-  .transform(to5ify)
+  browserify({ debug: true })
+  .transform(babelify)
+  .require('./src/index.js', { entry: true })
   .bundle()
-  //.pipe(uglify())
-  .pipe(source('game.js'))
-  .pipe(buffer())
-  .pipe(gulp.dest('./build'));
+  .on("error", function (err) { console.log("Error: " + err.message); })
+  .pipe(fs.createWriteStream("./build/game.js"));
 });
 
 // copy html, css, assets to build directory
