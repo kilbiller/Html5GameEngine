237ade735ab1c63c1f47d230e4a0bb9250fd4604
Rémy Peru <peru.remy@gmail.com>
2015-04-03 11:44:37 +0200

    fixed enemy health not showing on screen

--- src/systems/UI.js
+++ src/systems/UI.js
@@ -16,10 +16,7 @@ export default class UI extends SystemX {
       if(entity.type === "player") {
         if(!this.texts[entity.id]) {
           let playerHPBar = new PIXI.BitmapText("Health: " + ec.health.hp, {
-            font: "18px Consolas",
-            fill: "#FFFFFF",
-            stroke: "#000000",
-            strokeThickness: 3
+            font: "18px Consolas"
           });
           this.doc.addChild(playerHPBar);
           this.texts[entity.id] = playerHPBar;
@@ -37,8 +34,8 @@ export default class UI extends SystemX {
         }
         if(this.texts[entity.id]) {
           this.texts[entity.id].setText("Health: " + ec.health.hp);
-          this.texts[entity.id].x = ec.sprite.sprite.x + this.game.world.x;
-          this.texts[entity.id].y = ec.sprite.sprite.y + this.game.world.y - this.texts[entity.id].height;
+          this.texts[entity.id].x = ec.sprite.sprite.x + this.game.worldDoc.x;
+          this.texts[entity.id].y = ec.sprite.sprite.y + this.game.worldDoc.y - this.texts[entity.id].height;
 
           if(!ec.health.isAlive) {
             this.doc.removeChild(this.texts[entity.id]);
