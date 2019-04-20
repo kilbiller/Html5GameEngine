c260b280f4a19a55b45474e4641993bf1517452c
Rémy Peru <peru.remy@gmail.com>
2019-04-20 11:12:20 +0200

    Make it work again

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -4,15 +4,15 @@ import SpriteSheet from "../X/SpriteSheet";
 import Animations from "../X/Animations";
 
 // COMPONENTS
-import Position from "../Components/Position";
-import Dimension from "../Components/Dimension";
-import Sprite from "../Components/Sprite";
-import Animation from "../Components/Animation";
-import Health from "../Components/Health";
-import Collider from "../Components/Collider";
-import Velocity from "../Components/Velocity";
-import Attack from "../Components/Attack";
-import UserInput from "../Components/UserInput";
+import Position from "../components/Position";
+import Dimension from "../components/Dimension";
+import Sprite from "../components/Sprite";
+import Animation from "../components/Animation";
+import Health from "../components/Health";
+import Collider from "../components/Collider";
+import Velocity from "../components/Velocity";
+import Attack from "../components/Attack";
+import UserInput from "../components/UserInput";
 
 export default class Player extends Entity {
   constructor(x, y, width, height, textureName) {
@@ -27,72 +27,76 @@ export default class Player extends Entity {
 
     let spriteSheet = new SpriteSheet(textureName, width, height);
     super.addComponent(new Sprite(spriteSheet.getSprite()));
-    super.addComponent(new Animation(new Animations(spriteSheet, {
-      idleDown: {
-        frames: [0],
-        step: 0.15,
-        loop: true
-      },
-      idleUp: {
-        frames: [1],
-        step: 0.15,
-        loop: true
-      },
-      idleLeft: {
-        frames: [2],
-        step: 0.15,
-        loop: true
-      },
-      idleRight: {
-        frames: [3],
-        step: 0.15,
-        loop: true
-      },
-      moveDown: {
-        frames: [4, 5, 6, 7],
-        step: 0.15,
-        loop: true
-      },
-      moveUp: {
-        frames: [8, 9, 10, 11],
-        step: 0.15,
-        loop: true
-      },
-      moveLeft: {
-        frames: [12, 13, 14, 15],
-        step: 0.15,
-        loop: true
-      },
-      moveRight: {
-        frames: [16, 17, 18, 19],
-        step: 0.15,
-        loop: true
-      },
-      attackDown: {
-        frames: [20, 21, 22],
-        step: 0.1,
-        loop: false
-      },
-      attackUp: {
-        frames: [24, 25, 26],
-        step: 0.1,
-        loop: false
-      },
-      attackLeft: {
-        frames: [28, 29, 30],
-        step: 0.1,
-        loop: false
-      },
-      attackRight: {
-        frames: [32, 33, 34],
-        step: 0.1,
-        loop: false
-      },
-      death: {
-        frames: [36, 37, 38],
-        step: 0.12,
-        loop: false
-      }
-    })));
+    super.addComponent(
+      new Animation(
+        new Animations(spriteSheet, {
+          idleDown: {
+            frames: [0],
+            step: 0.15,
+            loop: true
+          },
+          idleUp: {
+            frames: [1],
+            step: 0.15,
+            loop: true
+          },
+          idleLeft: {
+            frames: [2],
+            step: 0.15,
+            loop: true
+          },
+          idleRight: {
+            frames: [3],
+            step: 0.15,
+            loop: true
+          },
+          moveDown: {
+            frames: [4, 5, 6, 7],
+            step: 0.15,
+            loop: true
+          },
+          moveUp: {
+            frames: [8, 9, 10, 11],
+            step: 0.15,
+            loop: true
+          },
+          moveLeft: {
+            frames: [12, 13, 14, 15],
+            step: 0.15,
+            loop: true
+          },
+          moveRight: {
+            frames: [16, 17, 18, 19],
+            step: 0.15,
+            loop: true
+          },
+          attackDown: {
+            frames: [20, 21, 22],
+            step: 0.1,
+            loop: false
+          },
+          attackUp: {
+            frames: [24, 25, 26],
+            step: 0.1,
+            loop: false
+          },
+          attackLeft: {
+            frames: [28, 29, 30],
+            step: 0.1,
+            loop: false
+          },
+          attackRight: {
+            frames: [32, 33, 34],
+            step: 0.1,
+            loop: false
+          },
+          death: {
+            frames: [36, 37, 38],
+            step: 0.12,
+            loop: false
+          }
+        })
+      )
+    );
   }
 }
